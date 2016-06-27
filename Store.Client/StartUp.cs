cc606ae21c0950993b47b3b7b6fa820d81b338e7
Petromil Pavlov <petromilpavlov@gmail.com>
2016-06-27 21:15:51 +0300

    [1]Added base structure.

---
+++ Store.Client/StartUp.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="Team Li Mei">
+//     Telerik Academy C# OOP Teamwork project.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Store.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The class StartUp holds the main starting point of our client.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Main starting point of out program.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
