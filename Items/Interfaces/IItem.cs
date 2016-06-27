a34021abdf5867e28bcaa0908beeae6ca2a21f1f
pspassova <petya.spassova@abv.bg>
2016-06-28 01:20:48 +0300

    Added an interface IItem.

--- Items/Interfaces/IItem.cs
+++ Items/Interfaces/IItem.cs
@@ -16,6 +16,10 @@
         {
             get;
         }
+
+        // I've made the expiration date of this type, so we can check if the item has expired, 
+        // by comparing it to DateTime.Now. 
+        // If you don't think we need this check, we can change it to a string maybe? 
         DateTime ExpirationDate
         {
             get;
