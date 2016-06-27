08481d977acb871169210f26c961c00a53da5950
pspassova <petya.spassova@abv.bg>
2016-06-28 01:25:59 +0300

    Added a class library for validation. Added a class for custom exceptions InvalidRangeException.

---
+++ Validation/InvalidRangeException.cs
@@ -0,0 +1,12 @@
+﻿namespace Validation
+{
+    using System;
+
+    public class InvalidRangeException : ApplicationException
+    {
+        public InvalidRangeException(int start, string message)
+            : base($"{message}\nValue should be a number bigger than {start}.")
+        {
+        }
+    }
+}
