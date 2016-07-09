6a94ac3750a5c2b06fdbe35c6bb11e81b557df0c
pspassova <petya.spassova@abv.bg>
2016-07-09 21:50:11 +0300

    Created a shopping-cart class.

---
+++ Store.Core/Models/Carts/Cart.cs
@@ -0,0 +1,35 @@
+﻿namespace Store.Core.Models.Carts
+{
+    using Store.Core.Contracts;
+    using System.Collections.Generic;
+
+    public class Cart
+    {
+        private List<IProduct> cart;
+
+        public Cart()
+        {
+            this.cart = new List<IProduct>();
+        }
+
+        public List<IProduct> Products
+        {
+            get
+            {
+                return new List<IProduct>(this.cart);
+            }
+        }
+        public void AddToCart(IProduct product)
+        {
+            this.cart.Add(product);
+        }
+        public bool RemoveFromCart(IProduct product)
+        {
+            return this.cart.Remove(product);
+        }
+        public void ClearCart(IProduct product)
+        {
+            this.cart.Clear();
+        }
+    }
+}
