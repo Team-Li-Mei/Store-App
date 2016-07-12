fea976ebaa3e1bbea02559062858582bcb8b3165
pspassova <petya.spassova@abv.bg>
2016-07-12 13:13:58 +0300

    Added new type of user.

---
+++ Store.Core/Models/Users/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Core.Models.Users
+{
+    using Abstract;
+    using Contracts;
+
+    public class Customer : User
+    {
+        public Customer(string setUsername, string setPassword, string setEmail)
+            : base(setUsername, setPassword, setEmail)
+        {
+        }
+        public override void AddItem(IProduct product)
+        {
+            base.AddItem(product);
+            this.Wallet.RemoveMoney(product.PricePerQuantity);
+        }
+    }
+}
