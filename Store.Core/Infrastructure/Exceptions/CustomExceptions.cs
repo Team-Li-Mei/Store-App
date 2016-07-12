68efce0ffaa4a143066ba8de9dcce1389f940b3d
molivpopov <mitkop@mail.bg>
2016-07-12 13:14:14 +0300

    More change to Constans

--- Store.Core/Infrastructure/Exceptions/CustomExceptions.cs
+++ Store.Core/Infrastructure/Exceptions/CustomExceptions.cs
@@ -1,14 +1,15 @@
 ﻿namespace Store.Core.Exceptions
 {
     using System;
+    using Store.Core.Infrastructure.Constants;
 
     public class NegativPriceAplicationException : Exception
     {
-        public NegativPriceAplicationException() : base("Negativ price is forbidden")
+        public NegativPriceAplicationException() : base(GeneralConstants.NegativPrice)
         {
             //new ("Negativ price is forbidden");
         }
 
-  
+
     }
 }
